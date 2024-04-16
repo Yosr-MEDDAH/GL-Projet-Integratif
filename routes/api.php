22ca53a05bb99c24c3564a715d013c115ccef201
Mohamed Amine Yeferni <yeferniamine35@gmail.com>
2024-04-16 20:14:26 +0100

    changement api.php ligne 104

--- routes/api.php
+++ routes/api.php
@@ -101,7 +101,7 @@ Route::controller(FournisseurAccessController::class)->group(function () {
     Route::post('/approuverFournisseur', 'accessFournisseur')->middleware('jwt.auth');
     Route::put('update/email/adress', 'updateEmail')->middleware('jwt.auth');
     Route::get('/fournisseursSansCompte', 'getFournisseurSansCompte')->middleware('jwt.auth');
-    Route::get('/fournisseursAvecCompte', 'getFournisseurAvecCompte')->middleware('jwt.auth');
+    Route::get('/fournisseursAvecCompte', 'getFournisseurAvecCompte')->middleware('jwt.auth'); // avec compte
 });
 
 Route::controller(BordoreauConsultation::class)->group(function () {
