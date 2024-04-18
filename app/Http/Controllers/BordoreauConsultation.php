96f57ac1d76a49cd8d5873f834d1245ea92e4a0f
Mohamed Amine Yeferni <yeferniamine35@gmail.com>
2024-04-18 18:29:03 +0100

    correction recherche bof et fournisseur PO

--- app/Http/Controllers/BordoreauConsultation.php
+++ app/Http/Controllers/BordoreauConsultation.php
@@ -26,7 +26,11 @@ class BordoreauConsultation extends Controller
         $page = $request->query('page', 1);
         $nb = $request->query('nb', 10);
 
-        $bordoreaux = Bordereau::select('date_sent', 'folder', 'status', 'nature', 'reference')->orderBy('created_at', 'desc')->paginate($nb, ['*'], 'page', $page);
+        $bordoreaux = Bordereau::select('id', 'date_sent', 'folder', 'status', 'nature', 'reference')->orderBy('created_at', 'desc')->paginate($nb, ['*'], 'page', $page);
+        foreach ($bordoreaux as $bordoreau) {
+            $factures = Facture::where("borderau_id", $bordoreau->id)->count();
+            $bordoreau->nombreFacture = $factures;
+        }
 
         return response()->json([
             'success' => true,
@@ -62,7 +66,7 @@ class BordoreauConsultation extends Controller
         }
         $page = $request->query('page', 1);
         $nb = $request->query('nb', 10);
-        $listFacture = Facture::where('borderau_id', $request->input('id'))->orderBy('created_at', 'desc')->paginate($nb, ['*'], 'page', $page);
+        $listFacture = Facture::select()->where('borderau_id', $request->input('id'))->orderBy('created_at', 'desc')->paginate($nb, ['*'], 'page', $page);
 
         return response()->json([
             'success' => true,
--- app/Http/Controllers/FiltreRechercheController.php
+++ app/Http/Controllers/FiltreRechercheController.php
@@ -98,10 +98,10 @@ class FiltreRechercheController extends Controller
         $page = $request->query('page', 1);
         $nb = $request->query('nb', 10);
         if ($role->id === 3) {
-            if ($request->input('hasInvoice') === 1) {
+            if ($request->input('hasInvoice') === "1") {
                 $purOrders = BonDeCommande::where('num_commande', 'LIKE', '%' . $request->input('search') . '%')
                     ->where('four_idFiscale', $user->idFiscale)->where('hasInvoice', 1)->paginate($nb, ['*'], 'page', $page);
-            } elseif ($request->input('hasInvoice') === 0) {
+            } elseif ($request->input('hasInvoice') === "0") {
                 $purOrders = BonDeCommande::where('num_commande', 'LIKE', '%' . $request->input('search') . '%')
                     ->where('four_idFiscale', $user->idFiscale)->where('hasInvoice', 0)->paginate($nb, ['*'], 'page', $page);
             } else {
@@ -147,14 +147,14 @@ class FiltreRechercheController extends Controller
 
         $page = $request->query('page', 1);
         $nb = $request->query('nb', 10);
-        if ($request->input('hasInvoice') === 1) {
+        if ($request->input('hasInvoice') === "1") {
             $purOrders = BonDeCommande::where(function ($query) use ($request) {
                 $query->where('num_commande', 'LIKE', '%' . $request->input('search') . '%')
                     ->orWhere('four_idFiscale', $request->input('search'));
             })
                 ->where('hasInvoice', 1)
                 ->paginate($nb, ['*'], 'page', $page);
-        } elseif ($request->input('hasInvoice') === 0) {
+        } elseif ($request->input('hasInvoice') === "0") {
             $purOrders = BonDeCommande::where(function ($query) use ($request) {
                 $query->where('num_commande', 'LIKE', '%' . $request->input('search') . '%')
                     ->orWhere('four_idFiscale', $request->input('search'));
