e80115634b3ac8971b922c6bb33ed765b06b6125
Mohamed Amine Yeferni <yeferniamine35@gmail.com>
2024-04-23 11:04:16 +0100

    ajouter une table types factures

---
+++ app/Models/TypesFactures.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class TypesFactures extends Model
+{
+    use HasFactory;
+}
